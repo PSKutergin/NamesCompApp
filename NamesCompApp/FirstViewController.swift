59d4dc4f50f3de1c7a3db88808829bc5b6127435
PSKutergin <PSKutergin@gmail.com>
2023-02-01 10:15:27 +0500

    second commit

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -12,11 +12,6 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var yourNameTF: UITextField!
     @IBOutlet weak var partnerNameTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? ResultViewController else { return }
         destinationVC.firstName = yourNameTF.text
@@ -24,7 +19,46 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func resultButtonTapped() {
+        guard let firstName = yourNameTF.text, let secondName = partnerNameTF.text else { return }
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(
+                title: "Names are missing",
+                message: "Please enter both names 😏")
+            return
+        }
         performSegue(withIdentifier: "goToResult", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToFirstVC(segue: UIStoryboardSegue) {
+//        guard segue.identifier == "unwindSegue" else {return}
+        
+        yourNameTF.text = ""
+        partnerNameTF.text = ""
+    }
+    
+}
+
+extension FirstViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
+extension FirstViewController: UITextViewDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resultButtonTapped()
+        }
+        return true
+    }
+}
